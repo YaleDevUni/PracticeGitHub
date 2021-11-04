922ac7931bd916089631194f1186ae825310dce3
Yeil Park <rhrnak4403@gmail.com>
2021-11-04 12:23:42 +0900

    second page first commit

---
+++ lib/second.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class secondpage extends StatefulWidget {
+  const secondpage({Key? key}) : super(key: key);
+
+  @override
+  _secondpageState createState() => _secondpageState();
+}
+
+class _secondpageState extends State<secondpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
